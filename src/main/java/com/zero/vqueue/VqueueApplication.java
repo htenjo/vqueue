96b4c6573936c977af5657320a51991e870fb9c7
Hernan <zerovirus23@gmail.com>
2020-07-04 20:48:21 -0500

    Creating the base project.

---
+++ src/main/java/com/zero/vqueue/VqueueApplication.java
@@ -0,0 +1,13 @@
+package com.zero.vqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VqueueApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(VqueueApplication.class, args);
+    }
+    
+}
